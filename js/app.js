c157853e38d5973530e41d06ff2a3343b02b6ec4
Matt Shultz <teamshultz@gmail.com>
2019-01-14 20:49:16 -0600

    Populate sidebar with locations

--- js/app.js
+++ js/app.js
@@ -10,13 +10,16 @@ class Location{
 
 var ViewModel = function() {
     var self = this;
-    self.restaurants = [
+    self.restaurants = ko.observableArray([
+        new Location('Cafe Du Monde', '29.957523', '-90.061862'),
+        new Location('Old Absinthe House', '29.955357', '-90.068444'),
+        new Location('Bourbon House', '29.954563', '-90.069131'),
         new Location('Meauxbar', '29.962700', '-90.066160'),
         new Location('Oceana Grill', '29.956270', '-90.067619'),
         new Location('Muriel\'s Jackson Square', '29.958490', '-90.063179'),
         new Location('Brennan\'s', '29.956240', '-90.066719'),
         new Location('Sylvain', '29.957100', '-90.064240')
-    ];
+    ]);
 };
 
 function init() {
@@ -44,9 +47,9 @@ function init() {
     var marker, i;
 
     // loop through restaurants and set up markers/infowindows
-    for (i = 0; i < vm.restaurants.length; i++) {  
+    for (i = 0; i < vm.restaurants().length; i++) {  
         marker = new google.maps.Marker({
-          position: new google.maps.LatLng(vm.restaurants[i].lat, vm.restaurants[i].long),
+          position: new google.maps.LatLng(vm.restaurants()[i].lat, vm.restaurants()[i].long),
           map: map,
           animation: google.maps.Animation.DROP
         });
@@ -57,7 +60,7 @@ function init() {
         // listen for click on marker, populate then show infoWindow
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
-                infoWindow.setContent(vm.restaurants[i].title);
+                infoWindow.setContent(vm.restaurants()[i].title);
                 infoWindow.open(map, marker);
             }
         })(marker, i));
