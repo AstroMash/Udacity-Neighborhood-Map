99ae069bf1332bcf3081e77a5e0957144a51e9d6
Matt Shultz <teamshultz@gmail.com>
2019-01-15 16:23:31 -0600

    Integrate Foursquare API for infowindows

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-var map;
-var restaurantsVisible = true;
+let map;
 
-var ViewModel = function() {
-    var self = this;
+const ViewModel = function() {
+    const self = this;
     self.restaurants = ko.observableArray(locations);
 
     // setup empty list filter
@@ -11,7 +10,7 @@ var ViewModel = function() {
     // this controls which locations display in the list and on the map
     // based on user input in the sidebar
     self.filteredRestaurants = ko.computed(function() {
-        var filter = self.query().toLowerCase();
+        const filter = self.query().toLowerCase();
         if(!filter) {
             // if filter query is empty, set all markers visible and return all restaurants
             ko.utils.arrayForEach(self.restaurants(), function(restaurant) {
@@ -38,9 +37,8 @@ var ViewModel = function() {
 };
 
 function init() {
-    // set ViewModel into a variable so we can access it's properties
-    const vm = new ViewModel();
     // initialize knockout bindings on ViewModel
+    const vm = new ViewModel();
     ko.applyBindings(vm);
 
     const frenchQuarter = {
@@ -55,19 +53,20 @@ function init() {
     });
 
     // initialize empty infoWindow
-    var infoWindow = new google.maps.InfoWindow();
+    const infoWindow = new google.maps.InfoWindow();
 
     // set up map boundary
-    var bounds = new google.maps.LatLngBounds();
+    const bounds = new google.maps.LatLngBounds();
 
-    var markers = [];
-    var marker, i;
+    const markers = [];
+    let marker, i;
 
     // loop through restaurants and set up markers/infowindows
     for (i = 0; i < vm.restaurants().length; i++) {  
         marker = new google.maps.Marker({
           position: new google.maps.LatLng(vm.restaurants()[i].lat, vm.restaurants()[i].long),
           map: map,
+          title: vm.restaurants()[i].title,
           animation: google.maps.Animation.DROP
         });
 
@@ -80,66 +79,60 @@ function init() {
         // extend boundary of map to fit new marker
         bounds.extend(marker.position);
   
-        // listen for click on marker, populate then show infoWindow
+        // listen for click on marker, show infowindow
         marker.addListener('click', (function(marker, i) {
             return function() {
                 // show the infowindow (which will trigger the bounce)
-                showInfoWindow(vm.restaurants()[i].title, map, marker);
+                showInfoWindow(map, marker);
             };
         })(marker, i));
     };
 
-    function showInfoWindow(title, map, marker) {
-        bounceMarker(marker);
-        infoWindow.setContent(title);
-        infoWindow.open(map, marker);
+    // tell the map to fit the new boundary set by markers
+    map.fitBounds(bounds);
+
+    this.showInfoWindow = function(map, marker) {
         
+        // query FourSquare API for extra data
+        const fsClientId = 'V03AWSI2OZSAX04CKR0IYX3LIJDLWXLLKWXIPNQJNPQXPL2V';
+        const fsClientSecret = 'BJASD2ONCPQMAQJS125BZ5JZHEWQ2CIUQQ5OA0IXVNT4AA3M';
+        const fsAPI = 'https://api.foursquare.com/v2/venues/search';
+
+        $.getJSON(fsAPI, {
+            ll: marker.position.lat() + ',' + marker.position.lng(),
+            intent: 'match',
+            name: marker.title,
+            limit: 1,
+            client_id: fsClientId,
+            client_secret: fsClientSecret,
+            v: '20190115'
+        })
+        .done(function(data) {
+            let response = data.response.venues[0];
+            marker.category = response.categories[0].shortName;
+            marker.streetAddress = response.location.formattedAddress[0];
+            marker.foursquareId = response.id;
+            marker.foursquareLink = "https://foursquare.com/v/" + marker.foursquareId;
+            console.log(response);
+            infoWindow.setContent(
+                '<p class="info info-title">' + marker.title + '</p>' +
+                '<p class="info info-street">' + marker.streetAddress + '</p>' +
+                '<p class="info info-category">' + marker.category + '</p>' + 
+                '<p class="info info-link"><a href="' + marker.foursquareLink + '?ref=' + fsClientId + '" target="_blank">View on Foursquare</a></p>' +
+                '<img class="info-attrib" src="img/powered-by-foursquare-grey-sm.png" alt="Powered by Foursquare">'
+            );
+            infoWindow.open(map, marker);
+        });
+
+        bounceMarker(marker);
     }
 
-    function bounceMarker(marker) {
+    this.bounceMarker = function(marker) {
             // MAKE IT BOUNCE (but only once)!
             marker.setAnimation(google.maps.Animation.BOUNCE);
             setTimeout(function() {
                 marker.setAnimation(-1);
             }, 700);
-        
-    }
-
-    // tell the map to fit the new boundary set by markers
-    map.fitBounds(bounds);
-
-    // listen for click on the show/hide restaurants buttons
-    document.querySelector("#toggle-all").addEventListener('click', toggleRestaurants);
-
-    // toggle restaurant visibility
-    function toggleRestaurants() {
-        if(restaurantsVisible === false) {
-            // toggle visibility variable
-            restaurantsVisible = true;
-            // clear search filter
-            vm.query('');
-            // loop through markers array, extend boundary of map, and show the marker
-            for (var i = 0; i < markers.length; i++) {
-                // reset any invisible markers (from filter function)
-                markers[i].setVisible(true);
-                // stop any bouncing markers
-                markers[i].setAnimation(-1);
-                markers[i].setMap(map);
-                bounds.extend(markers[i].position);
-            }
-            // close infowindow
-            infoWindow.close();
-            // tell the map to fit the new boundary set by markers
-            map.fitBounds(bounds);
-        } else {
-            // toggle visibility variable
-            restaurantsVisible = false;
-            for (var i = 0; i < markers.length; i++) {
-                // close infowindow
-                infoWindow.close();
-                markers[i].setMap(null);
-            };
-        }
-    }
+    };
 
 };
