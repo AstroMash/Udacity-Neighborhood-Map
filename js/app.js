ba836ce5899f21d00cc008a3f8e5a8c43aa9a61a
Matt Shultz <teamshultz@gmail.com>
2019-01-14 20:03:30 -0600

    Set up map markers and info windows

--- js/app.js
+++ js/app.js
@@ -1,13 +1,59 @@
 var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 29.9584, lng: -90.0644},
-        zoom: 13
-    });
-}
 
-var ViewModel = () => {
-    var self = this;
+class Location{
+    constructor(title, lat, long) {
+        this.title = title;
+        this.lat = lat;
+        this.long = long;
+    }
 };
 
-ko.applyBindings(new ViewModel());
+var ViewModel = function() {
+    var self = this;
+    self.restaurants = [
+        new Location('Meauxbar', '29.962700', '-90.066160'),
+        new Location('Meauxbar', '29.962700', '-90.066160'),
+        new Location('Oceana Grill', '29.956270', '-90.067619'),
+        new Location('Muriel\'s Jackson Square', '29.958490', '-90.063179'),
+        new Location('Brennan\'s', '29.956240', '-90.066719'),
+        new Location('Sylvain', '29.957100', '-90.064240')
+    ];
+};
+
+function init() {
+    // set ViewModel into a variable so we can access it's properties
+    const vm = new ViewModel();
+    // initialize knockout bindings on ViewModel
+    ko.applyBindings(vm);
+
+    const frenchQuarter = {
+        lat: 29.9584,
+        lng: -90.0644
+    }
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: frenchQuarter,
+        zoom: 15
+    });
+
+    // initialize empty infoWindow
+    var infoWindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    // loop through restaurants and set up markers/infowindows
+    for (i = 0; i < vm.restaurants.length; i++) {  
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(vm.restaurants[i].lat, vm.restaurants[i].long),
+          map: map
+        });
+  
+        // listen for click on marker, show infowindow
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(vm.restaurants[i].title);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+    };
+};
